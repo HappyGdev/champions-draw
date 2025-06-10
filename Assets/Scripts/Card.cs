202da23361c6a90064117c2357c7b4f7747d2f1f
Chaos King <Adel.gamedev@gmail.com>
2025-06-10 12:55:12 +0330

    Boss Card Added

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Card",menuName ="Cards" )]
+public enum CardActionType
+{
+    Attack,
+    Heal,
+    Multi,
+}
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Cards")]
 public class Card : ScriptableObject
 {
     public new string name;
@@ -9,9 +16,11 @@ public class Card : ScriptableObject
     public Sprite artwork;
     public Sprite type;
 
+    public bool isBoss;
 
-    public int value1;  //mana
-    public int value2;  //attack
-    public int value3;  //health
+    public int value1;  // mana
+    public int value2;  // attack
+    public int value3;  // health
 
+    public CardActionType actionType; // Add this line for enum usage
 }
